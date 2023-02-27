549a63bf930dd7fd6aaf271571f79b52e4aab919
TeodorNicorescu <TeodorNicorescu@DESKTOP-9Z86TQ3>
2023-02-27 18:46:12 +0200

    Add project files.

---
+++ KendoUIApp2/App_Start/BundleConfig.cs
@@ -0,0 +1,26 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace KendoUIApp2
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                    "~/Content/bootstrap.css",
+                    "~/Content/Site.css"));
+
+            bundles.Add(new ScriptBundle("~/Kendo").Include("~/Kendo/kendo.all.min.js", "~/Kendo/js/kendo.aspnetmvc.min.js"));
+            bundles.Add(new StyleBundle("~/Kendo/styles").Include("~/Kendo/kendo.common.min.css",
+                "~/Kendo/kendo.default.min.css"));
+        }
+    }
+}
---
+++ KendoUIApp2/Controllers/MyTestingController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using KendoUIApp2.Models;
+
+namespace KendoUIApp2.Controllers
+{
+    public class MyTestingController : Controller
+    {
+        // GET: MyTesting
+        private List<TestingModel> models = new List<TestingModel>
+        {
+            new TestingModel{Id = 14, Name = "object 14"},
+            new TestingModel{Id = 1, Name = "object 1"},
+            new TestingModel{Id = 2, Name = "object 2"},
+            new TestingModel{Id = 11, Name = "object 11"},
+            new TestingModel{Id = 3, Name = "object 3"},
+            new TestingModel{Id = 4, Name = "object 4"},
+            new TestingModel{Id = 5, Name = "object 5"},
+            new TestingModel{Id = 10, Name = "object 10"},
+            new TestingModel{Id = 9, Name = "object 9"},
+            new TestingModel{Id = 6, Name = "object 6"},
+            new TestingModel{Id = 12, Name = "object 12"},
+            new TestingModel{Id = 7, Name = "object 7"},
+            new TestingModel{Id = 8, Name = "object 8"},
+            new TestingModel{Id = 13, Name = "object 13"},
+        };
+
+        public ActionResult Testing()
+        {
+            ViewBag.Models = models;
+            return View();
+        }
+
+        public ActionResult GetProducts([DataSourceRequest] DataSourceRequest dataSourceRequest)
+        {
+            DataSourceResult result = models.ToDataSourceResult(dataSourceRequest);
+            return Json(result);
+        }
+    }
+}
